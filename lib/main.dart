a88dc07084c9349451b13e66cd9a5d5b188e4ae0
Kaparovvv <kaparovkanagat@gmail.com>
2023-02-26 15:07:32 +0600

    chore: file structure in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_family_flutter/app.dart';
+import 'package:my_family_flutter/application.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
