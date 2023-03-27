6cd2d1cc09faf92380b6b512015cf581cad06eb4
Ray <44358562+duldiev@users.noreply.github.com>
2023-03-27 22:20:11 +0500

    user model null type error fixed

--- lib/features/auth/domain/entity/user_auth_entity.dart
+++ lib/features/auth/domain/entity/user_auth_entity.dart
@@ -2,17 +2,28 @@ import 'package:equatable/equatable.dart';
 
 class UserAuthEntity extends Equatable {
   final String token;
+  final String id;
   final String phoneNumber;
-  final String fullName;
+  final String firstName;
+  final String lastName;
   final String roles;
 
   const UserAuthEntity({
+    required this.id,
     required this.token,
     required this.phoneNumber,
-    required this.fullName,
+    required this.firstName,
+    required this.lastName,
     required this.roles,
   });
 
   @override
-  List<Object?> get props => [token];
+  List<Object?> get props => [
+        token,
+        id,
+        phoneNumber,
+        firstName,
+        lastName,
+        roles,
+      ];
 }
