1ca124f46290ccc681711c88d54fbe85b354fb68
Ray <44358562+duldiev@users.noreply.github.com>
2023-04-08 00:12:52 +0500

    user enitity fields changed

--- lib/features/auth/domain/entity/user_auth_entity.dart
+++ lib/features/auth/domain/entity/user_auth_entity.dart
@@ -1,29 +1,17 @@
 import 'package:equatable/equatable.dart';
 
 class UserAuthEntity extends Equatable {
-  final String token;
   final String id;
-  final String phoneNumber;
-  final String firstName;
-  final String lastName;
-  final String roles;
+  final String token;
 
   const UserAuthEntity({
     required this.id,
     required this.token,
-    required this.phoneNumber,
-    required this.firstName,
-    required this.lastName,
-    required this.roles,
   });
 
   @override
   List<Object?> get props => [
         token,
         id,
-        phoneNumber,
-        firstName,
-        lastName,
-        roles,
       ];
 }
