e04f4ee68ea327558964ffca7f953f2ba1e41211
Ray <44358562+duldiev@users.noreply.github.com>
2023-04-10 22:05:41 +0500

    BT4-132

--- lib/core/widgets/payments_bottom_sheet_widget.dart
+++ lib/core/widgets/payments_bottom_sheet_widget.dart
@@ -40,130 +40,133 @@ class _PaymentsBottomSheetWidgetState extends State<PaymentsBottomSheetWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      clipBehavior: Clip.none,
-      children: [
-        Positioned(
-          top: 10,
-          child: Container(
-            width: context.width * 0.2,
-            height: 6,
-            decoration: BoxDecoration(
-              color: ThemeHelper.blueGrey100,
-              borderRadius: BorderRadius.circular(5),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: 10,
+            child: Container(
+              width: context.width * 0.2,
+              height: 6,
+              decoration: BoxDecoration(
+                color: ThemeHelper.blueGrey100,
+                borderRadius: BorderRadius.circular(5),
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-            top: 50,
-            left: 20,
-            right: 20,
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: context.width * 0.6,
-                  child: Text(
-                    widget.transaction,
-                    maxLines: 3,
-                    style: TextStyleHelper.f20w600,
-                    overflow: TextOverflow.ellipsis,
+          Padding(
+            padding: EdgeInsets.only(
+              top: 50,
+              left: 20,
+              right: 20,
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: context.width * 0.6,
+                    child: Text(
+                      widget.transaction,
+                      maxLines: 3,
+                      style: TextStyleHelper.f20w600,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 25),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: TextHelper.userBalance.toLowerCase(),
-                        style: TextStyleHelper.f18w500
-                            .copyWith(fontWeight: FontWeight.w400),
-                      ),
-                      const WidgetSpan(
-                        child: SizedBox(
-                          width: 10,
+                  const SizedBox(height: 25),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: TextHelper.userBalance.toLowerCase(),
+                          style: TextStyleHelper.f18w500
+                              .copyWith(fontWeight: FontWeight.w400),
                         ),
-                      ),
-                      TextSpan(
-                        text: balance.toString(),
-                        style: TextStyleHelper.f20w600
-                            .copyWith(color: ThemeHelper.color08B89D),
-                      ),
-                    ],
-                  ),
-                ),
-                _isDontEnoughFunds
-                    ? Padding(
-                        padding: const EdgeInsets.only(top: 5),
-                        child: Text(
-                          TextHelper.dontEnoughFundsance,
-                          style: TextStyleHelper.f14w600
-                              .copyWith(color: ThemeHelper.red),
+                        const WidgetSpan(
+                          child: SizedBox(
+                            width: 10,
+                          ),
                         ),
-                      )
-                    : const SizedBox(),
-                const SizedBox(height: 30),
-                const Text(
-                  TextHelper.recipient,
-                  style: TextStyleHelper.f14w600,
-                ),
-                const SizedBox(height: 10),
-                CustomTextFieldWidget(
-                  controller: _recepientController,
-                  enabled: false,
-                  value: widget.recepient,
-                  validate: (value) => ValidatesHelper.titleValidate(
-                    value!,
-                    TextHelper.recipient.toLowerCase(),
+                        TextSpan(
+                          text: balance.toString(),
+                          style: TextStyleHelper.f20w600
+                              .copyWith(color: ThemeHelper.color08B89D),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                const Text(
-                  TextHelper.enterThePaymentSum,
-                  style: TextStyleHelper.f14w600,
-                ),
-                const SizedBox(height: 10),
-                CustomTextFieldWidget(
-                  controller: _sumController,
-                  keyboardType: TextInputType.number,
-                  label: TextHelper.sum,
-                  textInputAction: TextInputAction.done,
-                  value: widget.amountToBePaid,
-                  validate: (value) => ValidatesHelper.titleValidate(
-                    value!,
-                    TextHelper.enterThePaymentSum.toLowerCase(),
+                  _isDontEnoughFunds
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: Text(
+                            TextHelper.dontEnoughFundsance,
+                            style: TextStyleHelper.f14w600
+                                .copyWith(color: ThemeHelper.red),
+                          ),
+                        )
+                      : const SizedBox(),
+                  const SizedBox(height: 30),
+                  const Text(
+                    TextHelper.recipient,
+                    style: TextStyleHelper.f14w600,
                   ),
-                  onChanged: (value) {
-                    _onChangedSum(double.parse(value));
-                  },
-                ),
-                const SizedBox(height: 40),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: CustomElevatedButtonWidget(
-                    title: TextHelper.toPay,
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _comparisonWithBalance(
-                          double.parse(_sumController.text),
-                        );
-                      }
+                  const SizedBox(height: 10),
+                  CustomTextFieldWidget(
+                    controller: _recepientController,
+                    enabled: false,
+                    value: widget.recepient,
+                    validate: (value) => ValidatesHelper.titleValidate(
+                      value!,
+                      TextHelper.recipient.toLowerCase(),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(
+                    TextHelper.enterThePaymentSum,
+                    style: TextStyleHelper.f14w600,
+                  ),
+                  const SizedBox(height: 10),
+                  CustomTextFieldWidget(
+                    controller: _sumController,
+                    keyboardType: TextInputType.number,
+                    label: TextHelper.sum,
+                    textInputAction: TextInputAction.done,
+                    value: widget.amountToBePaid,
+                    validate: (value) => ValidatesHelper.titleValidate(
+                      value!,
+                      TextHelper.enterThePaymentSum.toLowerCase(),
+                    ),
+                    onChanged: (value) {
+                      _onChangedSum(double.parse(value));
                     },
                   ),
-                ),
-                const SizedBox(height: 30),
-              ],
+                  const SizedBox(height: 40),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: CustomElevatedButtonWidget(
+                      title: TextHelper.toPay,
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _comparisonWithBalance(
+                            double.parse(_sumController.text),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
